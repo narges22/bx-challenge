4e7c5464e4c6dd0ca395e50f786569ea6e706e3b
NargesSh <narges_shaker@ymail.com>
2026-02-10 18:44:20 +0000

    redux store added

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import { Main } from './main'
 import './styles/global.scss'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Main />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>
+  </Provider>
 )
---
+++ src/store/themeSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const getInitialTheme = () => {
+  const savedTheme = localStorage.getItem('theme')
+  if (savedTheme) {
+    return savedTheme
+  }
+  return 'light'
+}
+
+const themeSlice = createSlice({
+  name: 'theme',
+  initialState: {
+    value: getInitialTheme()
+  },
+  reducers: {
+    setTheme: (state, action) => {
+      state.value = action.payload
+      localStorage.setItem('theme', action.payload)
+    }
+  }
+})
+
+export const { setTheme } = themeSlice.actions
+export default themeSlice.reducer
